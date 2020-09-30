b727965e5bfaaba7f99e4d7c117db7c685679263
Megan Orton <megan.orton0@gmail.com>
2020-09-29 20:39:30 -0500

    Add project files.

---
+++ Ch2 Studio/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Ch2_Studio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter a radius: ");
+            string radius = Console.ReadLine();
+
+            double area = Math.PI * Math.Pow(double.Parse(radius), 2);
+            double circumference = 2 * Math.PI * double.Parse(radius);
+            
+            Console.WriteLine("The area of a circle with radius " + radius + " is: " + area);
+            Console.WriteLine("The circumference of a circle with radius " + radius + " is: " + circumference);
+
+            Console.WriteLine("How many miles per gallon does your car get?");
+            string milesPerGallon = Console.ReadLine();
+            double gallons = circumference / double.Parse(milesPerGallon);
+
+            Console.WriteLine("Your car will use " + gallons + " gallons of gas to travel the circumference of a circle with radius " + radius);
+        }
+    }
+}
